3ae6e61760cd8cd8b8c005d05cc6751d5c66c3d2
gbeland <gbeland@gmail.com>
2022-08-31 12:43:35 -0600

    lint5

--- examples/DHRobotDualUartExample.py
+++ examples/DHRobotDualUartExample.py
@@ -24,14 +24,14 @@ iic_uart2 = DualUart.DFRobot_IIC_Serial(
 )
 
 try:
-    iic_uart1.begin(baud=9600, serialFormat=iic_uart1.IIC_Serial_8N1)
+    iic_uart1.begin(9600, iic_uart1.IIC_Serial_8N1)
     print("Opened: UART 1 ")
 except Exception as e:
     iic_uart1 = None
     print("Error: Could not open UART 1 Exception: " + str(e))
 
 try:
-    iic_uart2.begin(baud=9600, serialFormat=iic_uart2.IIC_Serial_8N1)
+    iic_uart2.begin(9600, iic_uart2.IIC_Serial_8N1)
     print("Opened: UART 2")
 except Exception as e:
     iic_uart2 = None
@@ -41,7 +41,7 @@ sendID = 1
 sendDelayCount = 1
 
 while True:
-    time.sleep(.3)
+    time.sleep(0.3)
     sendDelayCount -= 1
     if sendDelayCount <= 0:
         sendDelayCount = 10
@@ -60,6 +60,6 @@ while True:
         if iic_uart2.available():
             s = ""
             while iic_uart2.available():
-                b = (iic_uart2.read(1))
+                b = iic_uart2.read(1)
                 s += chr(b[0])
             print("<2:" + s + " len:" + str(len(s)) + ">")
