ac06e87304f2c4be87394ce22ff513670961a65b
gbeland <70548834+gbeland@users.noreply.github.com>
2022-08-31 13:43:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,18 +24,18 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     # Community Bundle Information
-    name="circuitpython_dfrobot_gravity_drf0627_i2c_dual_uart",
+    name="circuitpython_dfrobot_gravity_drf0627_dual_uart",
     use_scm_version={
         # This is needed for the PyPI version munging in the Github Actions release.yml
         "git_describe_command": "git describe --tags --long",
         "local_scheme": "no-local-version",
     },
     setup_requires=["setuptools_scm"],
-    description="CircuitPython library for the DF DFRobot_Gravity_DRF0627_I2C_Dual_Uart",
+    description="CircuitPython library for the DF DFRobot_Gravity_DRF0627_Dual_Uart",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     # The project's main homepage.
-    url="https://github.com/gbeland/CircuitPython_DFRobot_Gravity_DRF0627_I2C_Dual_Uart.git",
+    url="https://github.com/gbeland/CircuitPython_DFRobot_Gravity_DRF0627_Dual_Uart.git",
     # Author details
     author="Graham Beland",
     author_email="gbeland@gmail.com",  # TODO: Add your email here
@@ -55,12 +55,12 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     # What does your project relate to?
-    keywords="adafruit blinka circuitpython micropython rs232 uart serail rs485 "
+    keywords="adafruit blinka circuitpython micropython rs232 uart serial rs485 "
     "serial communications i2c iot original "
     "uart rs232 rs485 qwiic stemma blinkaCircuit",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
     #       CHANGE `py_modules=['...']` TO `packages=['...']`
-    py_modules=["circuitpython_dfrobot_gravity_drf0627_i2c_dual_uart"],
+    py_modules=["circuitpython_dfrobot_gravity_drf0627_dual_uart"],
 )
