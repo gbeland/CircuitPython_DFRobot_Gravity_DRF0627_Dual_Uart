73261678b9c95d1f301444195d36ec7fc0479679
gbeland <gbeland@gmail.com>
2022-08-31 15:51:04 -0600

    t4

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-# SPDX-FileCopyrightText: Copyright (c) 2022 Graham Beland
+# SPDX-FileCopyrightText: Copyright (c) 2022 Graham N Beland
 #
 # SPDX-License-Identifier: MIT
 
